411674bba9b6e44663a9fcac4870d4f7864bb02d
up1 <somkiat.p@gmail.com>
2022-09-02 15:52:14 +0700

    Trey to change

--- src/main/java/com/example/demospring/DemoSpringApplication.java
+++ src/main/java/com/example/demospring/DemoSpringApplication.java
@@ -8,7 +8,7 @@ public class DemoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
-		System.out.println("Hello world 3");
+		System.out.println("Hello world 4");
 	}
 
 }

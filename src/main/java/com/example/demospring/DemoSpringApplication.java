d3986c71c4f4c60442f7d746daa74ca58a8ea89b
up1 <somkiat.p@gmail.com>
2022-09-02 15:14:57 +0700

    Try to change code 1

--- src/main/java/com/example/demospring/DemoSpringApplication.java
+++ src/main/java/com/example/demospring/DemoSpringApplication.java
@@ -8,6 +8,7 @@ public class DemoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
